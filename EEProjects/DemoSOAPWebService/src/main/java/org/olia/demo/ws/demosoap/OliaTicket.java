809656c8c1f0a2da568f59769e8b752e1886cfd3
AlexYakushev <hrip85@gmail.com>
2017-09-02 10:08:24 +0300

    [+] New demo WebServices.

---
+++ EEProjects/DemoSOAPWebService/src/main/java/org/olia/demo/ws/demosoap/OliaTicket.java
@@ -0,0 +1,62 @@
+package org.olia.demo.ws.demosoap;
+
+import java.io.Serializable;
+
+import org.json.JSONObject;
+
+public class OliaTicket implements Serializable {
+	private static final long serialVersionUID = -2772919191683916141L;
+	
+	private String id;
+	private int num;
+	private String name;
+	private String description;
+	private String priority;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getPriority() {
+		return priority;
+	}
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public JSONObject toJSON() {
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("ID", id);
+		jsonObject.put("Number", num);
+		jsonObject.put("Name", name);
+		jsonObject.put("Description", description);
+		jsonObject.put("Priority", priority);
+		
+		return jsonObject;
+	}
+	
+	public String toString() {
+		return toJSON().toString();
+	}
+}
+
