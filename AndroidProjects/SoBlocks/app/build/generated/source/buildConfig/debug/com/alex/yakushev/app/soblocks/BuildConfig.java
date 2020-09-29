0e64f5aee7d3164afe7ec23ce171cc0bb06123ed
oleks <hrip85@gmail.com>
2020-09-29 15:42:21 +0300

    update build config for SoBlocks

---
+++ AndroidProjects/SoBlocks/app/build/generated/source/buildConfig/debug/com/alex/yakushev/app/soblocks/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alex.yakushev.app.soblocks;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.alex.yakushev.app.soblocks";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
