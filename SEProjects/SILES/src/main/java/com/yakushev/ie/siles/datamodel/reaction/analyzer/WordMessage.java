8467975e410ce91c73b59da6057f52faace937e8
oleks <hrip85@gmail.com>
2020-09-29 15:55:42 +0300

    remove old SILES project

--- SEProjects/SILES/src/main/java/com/yakushev/ie/siles/datamodel/reaction/analyzer/WordMessage.java
+++
@@ -1,8 +0,0 @@
-package com.yakushev.ie.siles.datamodel.reaction.analyzer;
-
-import java.util.ArrayList;
-
-public class WordMessage extends ArrayList<WordIdea> {
-	private static final long serialVersionUID = -1957354161273624064L;
-
-}
