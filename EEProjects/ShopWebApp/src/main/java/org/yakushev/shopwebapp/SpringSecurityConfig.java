645408f47f854b4693170ab1bb90e10c687b48cc
yakushev85 <hrip85@dinamicka.com>
2022-05-01 09:52:50 +0300

    add cors disabled by default

--- EEProjects/ShopWebApp/src/main/java/org/yakushev/shopwebapp/SpringSecurityConfig.java
+++ EEProjects/ShopWebApp/src/main/java/org/yakushev/shopwebapp/SpringSecurityConfig.java
@@ -25,7 +25,7 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors().and().csrf().disable()
                 .addFilterAt(new JwtCsrfFilter(jwtTokenRepository, resolver), CsrfFilter.class)
                 .authorizeRequests()
                 .anyRequest()
