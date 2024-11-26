c52798ed94205eee56d021a78d32804aabce3b56
Alex Yakushev <hrip85@gmail.com>
2024-11-26 06:14:17 +0200

    refactor, add docs

--- ShopWebApp/README.md
+++ ShopWebApp/README.md
@@ -12,6 +12,7 @@ Run docker using `docker compose up` in root folder.
 
 ## Test
 
-Open in browser http://0.0.0.0:8080/ or http://localhost:8080/.
-
+Open in browser http://0.0.0.0:8080/ or http://localhost:8080/
+Swagger documentation: http://localhost:8080/swagger-ui/index.html
+OpenAPI documentation: http://localhost:8080/docs or http://localhost:8080/docs.yaml
 To create admin user just sign up with `admin` username.
--- ShopWebApp/src/main/java/org/yakushev/shopwebapp/SpringSecurityConfig.java
+++ ShopWebApp/src/main/java/org/yakushev/shopwebapp/SpringSecurityConfig.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -34,10 +33,6 @@ public class SpringSecurityConfig {
                 )
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable);
-
-        // http.cors();
-
-        // http.csrf().disable();
 
         return http.build();
     }
